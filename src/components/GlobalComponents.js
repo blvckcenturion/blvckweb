520ad5cf9bc67598ea609a01be41843bb022dd3f
blvckcenturion <mr.plus.co@gmail.com>
2021-06-24 21:01:05 -0400

    Global Components V1

--- src/components/GlobalComponents.js
+++ src/components/GlobalComponents.js
@@ -8,37 +8,37 @@ export const MediumScreen = width >= 768
 export const BigScreen = width >= 1000
 
 export const H1 = styled(motion.h1)`
-font-size: 3.75vw; 
+font-size: 3.75vmax; 
 font-family: 'Extended Main';
 color: ${props => props.color ? props.color : "var(--blvck)"};
 @media (min-width: 768px) and (max-width: 1050px){
-    font-size: 4vw;
+    font-size: 4.5vmax;
 }
 @media (max-width: 767px){
-    font-size: 7vw;
+    font-size: 3.5vmax;
 }
 `
 
 export const H2 = styled(motion.h2)`
-    font-size: 1.75vw;
+    font-size: 1.75vmax;
     font-family: 'Extended Main';
     color: ${props => props.color ? props.color : "var(--green)"};
-    @media (min-width: 768px) and (max-width: 1050px){
-        font-size: 2vw;
+    @media (max-width: 1050px){
+        font-size: 2vmax;
     }
     @media (max-width: 767px){
-        font-size: 3.75vw;
+        font-size: 1.75vmax;
     }
 `
 export const H3 = styled(motion.h3)`
-    font-size: 2vw;
+    font-size: 2vmax;
     font-family: 'Extended Main';
     color: ${props => props.color ? props.color : "var(--blvck)"};
-    @media (min-width: 768px) and (max-width: 1050px){
-        font-size: 2.5vw;
+    @media (max-width: 1050px){
+        font-size: 2.5vmax;
     }
     @media (max-width: 767px){
-        font-size: 4.5vw;
+        font-size: 2vmax;
     }
 `
 
@@ -59,16 +59,16 @@ letter-spacing: 2px;
 margin: 0;
 `
 export const P = styled.p`
-font-size: 2.3vw;
+font-size: 2.3vmax;
 font-family: 'SFProDisplay';
 text-align: justify;
 letter-spacing: 5px;
 margin: 0;
 @media (min-width: 768px) and (max-width: 1050px){
-    font-size: 3.5vw;
+    font-size: 2.8vmax;
 }
 @media (max-width: 767px){
-    font-size: 4.5vw;
+    font-size: 2.5vmax;
 }
 `
 
@@ -88,34 +88,61 @@ export const AnimatedLogo = () => {
 
 export const defaultTransition = { stiffness: 1000, type: "tween" }
 
-export const CircularButton = ({children, onClick}) => {
+export const CircularButton = ({children, onClick, color}) => {
     const Button = styled(motion.button)`
         border: none;
         background-color: transparent;
-        font-size: 1.5rem;
-        width: 15vw;
-        height: 15vw;
+        width: 12vmax;
+        height: 12vmax;
         border-radius: 50%;
-
         display: flex;
         align-items: center;
         justify-content: center;
+        @media (max-width: 768px){
+            width: 16vmax;
+            height: 16vmax;
+        }
+        @media (max-width: 1050px) and (min-width: 768px){
+            width: 12vmax;
+            height: 12vmax;
+        }
     `
-    const ButtonText = styled.p`
-        height: 15vw;
+    const ButtonText = styled(motion.p)`
+        height: 12vmax;
         position: absolute;
-        color: ${props => props.color ? props.color : "var(--blvck)"};
+        font-size: 2vmax;
+        color: ${props => props.color ? props.color : "var(--green)"};
         transform: rotate(${props => props.deg}deg);
-        // transform-origin: 0 100%;
         margin: 0;
-        
+        @media (max-width: 767px){
+            height: 16vmax;
+            font-size: 2.5vmax;
+
+        }
+        @media (max-width: 1050px) and (min-width: 768px){
+            height: 12vmax;
+            font-size: 1.2rem;
+        }
     `
+    
+    const H4 = styled.h4`
+        font-size: 1.3vmax;
+        font-family: 'Extended Main';
+        color: ${props => props.color ? props.color : "var(--green)"};
+        @media (max-width: 767px){
+            font-size: 2vmax;
+        }
+    `
+
     const deg = 360 / children.length
     return (
-        <Button animate={{rotate:[0,-360]}} transition={{ duration: 6, repeatType: "loop", repeatDelay: 0, repeat: Infinity }} onClick={() => console.log('sapo')}>
+        <Button onClick={() => console.log('sapo')}>
+            <Div height={"1%"} width={"1%"} animate={{rotate:[0,-360]}} transition={{ duration: 6, repeatType: "loop", repeatDelay: 0, repeat: Infinity }}  style={{position: "absolute"}}>
             {children.split("").map((e,i) => {
-                return <ButtonText key={i} deg={`${i*deg}`}>{e}</ButtonText>
+                return <ButtonText color={color} key={i} deg={`${i*deg}`}>{e}</ButtonText>
             })}
+            </Div>
+            <H4  color={color} >Toca<br/> Aqui</H4>
         </Button>
     )
 }
